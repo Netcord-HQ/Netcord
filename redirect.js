8e7f22317f532e4707ca08261c69ec95dfb180aa
Chris Maven <scemerjr.0001@gmail.com>
2023-05-12 11:37:24 +0530

    BLOG Redirect

---
+++ redirect.js
@@ -0,0 +1,6 @@
+// Check if the current URL matches the desired pattern
+if (window.location.hostname === 'netcord.in' && window.location.pathname === '/blog') {
+    // Modify the URL and redirect
+    window.location.href = 'https://netcord.in';
+  }
+  
