8c7cf1cf0122c0cbfc1bed07beba90ae98326eec
Chris Maven <scemerjr.0001@gmail.com>
2023-05-15 11:35:53 +0530

    Link Change

--- htmlremoval.js
+++
@@ -1,9 +0,0 @@
-
-if (window.location.pathname.endsWith('.html')) {
-    // Remove the '.html' extension from the URL
-    var newUrl = window.location.pathname.slice(0, -5);
-    
-    // Use History API to replace the current URL without the '.html' extension
-    window.history.replaceState({}, '', newUrl);
-  }
-
